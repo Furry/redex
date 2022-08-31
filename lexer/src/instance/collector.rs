daa02416fa632d8f6c4ae2f0f874eaae2ff77589
Aurora <aurora@localhost.localdomain>
2022-08-31 10:19:43 -0400

    push

--- lexer/src/instance/collector.rs
+++ lexer/src/instance/collector.rs
@@ -2,7 +2,20 @@ use super::Instance;
 
 enum CollectorState {
     String,
-    Numeric
+    Numeric,
+    Identifier,
+    Whitespace,
+    Comment,
+    Decimal
+}
+
+pub enum Token {
+    String(String),
+    Numeric(String),
+    Identifier(String),
+    Whitespace(String),
+    Comment(String),
+    Decimal(String)
 }
 
 pub struct Collector {
@@ -10,7 +23,13 @@ pub struct Collector {
 }
 
 impl Collector {
-    pub fn tokenize<S: Into<String>>(&mut self, instance: &mut Instance, input: S) {
+    pub fn tokenize<S: Into<String>>(&mut self, instance: &mut Instance, input: S) -> Vec<Token> {
         let input: String = input.into();
+        let cache: String = String::new();
+
+        for (i, c) in input.chars().enumerate() {
+        }
+
+        todo!()
     }
 }
